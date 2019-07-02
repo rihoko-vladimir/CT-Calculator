e2e794f96af0bb7e90c253b97f5bf6a289ee2f50
Vladimir <vovakozlouskiy@gmail.com>
2019-07-03 01:41:53 +0300

    bugfixes

--- polling.py
+++ polling.py
@@ -15,42 +15,42 @@ itembtn9 = types.KeyboardButton('ЦТ по Истории Беларуси')
 itembtn10 = types.KeyboardButton('ЦТ по Географии')
 itembtn11 = types.KeyboardButton('ЦТ по Всемирной Истории')
 itembtn12 = types.KeyboardButton('Помощь')
-@bot.message_handler(func=lambda message: message.text == 'Помощь' or message.text == '/start' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'помощь' or message.text.lower() == '/start' and message.content_type == 'text')
 def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(row_width=1)
     markup.add(itembtn1, itembtn2, itembtn3, itembtn4 , itembtn5 , itembtn6 , itembtn7 ,itembtn8 ,itembtn9 , itembtn10 , itembtn11)
     bot.reply_to(message, "Привет! Вот список доступных команд.", reply_markup=markup)
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Русскому языку' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по русскому языку' and message.content_type == 'text')
 def rusct(message):
     bot.reply_to(message, calculate.calculate("/rus_ct"))
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Математике' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по математике' and message.content_type == 'text')
 def mathct(message):
     bot.reply_to(message, calculate.calculate("/math_ct"))
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Физике' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по физике' and message.content_type == 'text')
 def physct(message):
     bot.reply_to(message, calculate.calculate("/phys_ct"))
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Белорусскому языку' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по белорусскому языку' and message.content_type == 'text')
 def byct(message):
     bot.reply_to(message, calculate.calculate("/by_ct"))
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Обществоведению' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по обществоведению' and message.content_type == 'text')
 def obsct(message):
     bot.reply_to(message, calculate.calculate("/com_ct"))
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Биологии' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по биологии' and message.content_type == 'text')
 def biosct(message):
     bot.reply_to(message, calculate.calculate("/bio_ct"))
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Иностранному языку' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по иностранному языку' and message.content_type == 'text')
 def inct(message):
     bot.reply_to(message, calculate.calculate("/forlang_ct"))
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Географии' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по географии' and message.content_type == 'text')
 def geoct(message):
     bot.reply_to(message, calculate.calculate("/geo_ct"))
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Всемирной Истории' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по всемирной Истории' and message.content_type == 'text')
 def histct(message):
     bot.reply_to(message, calculate.calculate("/hist_ct"))
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Химии' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по химии' and message.content_type == 'text')
 def chemct(message):
     bot.reply_to(message, calculate.calculate("/chem_ct"))
-@bot.message_handler(func=lambda message: message.text == 'ЦТ по Истории Беларуси' and message.content_type == 'text')
+@bot.message_handler(func=lambda message: message.text.lower() == 'цт по истории беларуси' and message.content_type == 'text')
 def by_histct(message):
     bot.reply_to(message, calculate.calculate("/by_hist_ct"))
 
