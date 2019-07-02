351f1d12b645a3822aadb0a180b876fcf02e56d1
Vladimir <vovakozlouskiy@gmail.com>
2019-07-03 00:37:08 +0300

    Timezone hotfix

--- calculate.py
+++ calculate.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from dateutil.relativedelta import relativedelta as RD
 
 
@@ -15,7 +15,7 @@ by_hist_ct = datetime.strptime("2020/6/27/11/0/0", f)
 geo_ct = datetime.strptime("2020/6/29/11/0/0", f)
 hist_ct = datetime.strptime("2020/7/1/11/0/0", f)
 def calculate(message):
-    cur_time = datetime.now(timezone.utc)
+    cur_time = datetime.now()
     if message == '/phys_ct':
         yleft = RD(phys_ct, cur_time).years
         Mleft = RD(phys_ct, cur_time).months
